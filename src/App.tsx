3336c556500f6c706987847a6db5ec814b4d2714
Maciek <msitkowski94@gmail.com>
2021-08-08 10:45:29 +0200

    feat: display fetched book details

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+
+type Book = {
+  title: string;
+  author: string;
+  pages: number;
+  tags: string[];
+};
 
 function App() {
+  const [input, setInput] = React.useState("");
+  const [book, setBook] = React.useState<Book | null>(null);
+  const [error, setError] = React.useState<Error>(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(`http://localhost:5000/?url=${input}`)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error("Something went wrong");
+        }
+        return res;
+      })
+      .then((res) => res.json())
+      .then((json) => {
+        setBook(json);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="url"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.currentTarget.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
+        {error && <p>{error}</p>}
       </header>
+      <main>
+        {book && (
+          <>
+            <h2>Your book:</h2>
+            <h3>Title: {book.title}</h3>
+            <h3>Author: {book.author}</h3>
+            <h3>Pages: {book.pages}</h3>
+            <h3>Tags:</h3>
+            <ul>
+              {book.tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+              ))}
+            </ul>
+          </>
+        )}
+      </main>
     </div>
   );
 }
