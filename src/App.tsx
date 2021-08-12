8d8402fa6a634c0a2b534c9a7fb1235ca5445c1a
Maciek <msitkowski94@gmail.com>
2021-08-12 19:34:11 +0200

    style: format all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import "./App.css";
+import * as React from 'react';
+import './App.css';
 
 type Book = {
   title: string;
@@ -7,38 +7,38 @@ type Book = {
   pages: number;
   tags: string[];
 };
-const dbId = "ff8e1c59e0124c9db5ac282b9a5c77fa";
+const dbId = 'ff8e1c59e0124c9db5ac282b9a5c77fa';
 function App() {
-  const [input, setInput] = React.useState("");
+  const [input, setInput] = React.useState('');
   const [book, setBook] = React.useState<Book | null>(null);
   const [error, setError] = React.useState<Error>(null);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
 
     fetch(`http://localhost:5000/?url=${input}`)
-      .then((res) => {
+      .then(res => {
         if (res.status !== 200) {
-          throw new Error("Something went wrong");
+          throw new Error('Something went wrong');
         }
         return res;
       })
-      .then((res) => res.json())
-      .then((json) => {
+      .then(res => res.json())
+      .then(json => {
         setBook(json);
       })
-      .catch((error) => {
+      .catch(error => {
         setError(error.message);
       });
   };
 
   const createBook = () => {
     console.log(book);
-    fetch("http://localhost:5000/book", {
-      method: "POST",
+    fetch('http://localhost:5000/book', {
+      method: 'POST',
       body: JSON.stringify(book),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
       .then(console.log)
@@ -53,7 +53,7 @@ function App() {
             name="url"
             type="text"
             value={input}
-            onChange={(e) => setInput(e.currentTarget.value)}
+            onChange={e => setInput(e.currentTarget.value)}
           />
           <button type="submit">Send</button>
         </form>
@@ -68,7 +68,7 @@ function App() {
             <h3>Pages: {book.pages}</h3>
             <h3>Tags:</h3>
             <ul>
-              {book.tags.map((tag) => (
+              {book.tags.map(tag => (
                 <li key={tag}>{tag}</li>
               ))}
             </ul>
