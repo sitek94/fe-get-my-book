8e8b9b901d5bd630d0add35597c7ccbf72a1f212
Maciek <msitkowski94@gmail.com>
2021-08-14 12:06:23 +0200

    feat(booksApi): create 'addBook' method to add the book to DB

--- src/api/books-api.ts
+++ src/api/books-api.ts
@@ -2,7 +2,10 @@ import { Book } from '../types';
 import http from './http';
 
 const booksApi = {
+  // TODO: Probably more suitable method name would be "scrapeBook" as this
+  // is what is actually happening there
   getBook,
+  addBook,
 };
 
 /**
@@ -14,6 +17,15 @@ async function getBook(url: string) {
       url,
     },
   });
+
+  return res.data;
+}
+
+/**
+ * Adds the book to Notion DB
+ */
+async function addBook(book: Book) {
+  const res = await http.post<Book>('/book', book);
 
   return res.data;
 }
